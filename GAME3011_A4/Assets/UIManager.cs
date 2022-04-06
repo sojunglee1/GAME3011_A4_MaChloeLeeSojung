33b75fbb1c58005c4084358a38d9ba068829a044
Sojung Lee <sojung.lee2@georgebrown.ca>
2022-04-06 13:57:33 -0400

    Updated HUD text with info

--- GAME3011_A4/Assets/UIManager.cs
+++ GAME3011_A4/Assets/UIManager.cs
@@ -8,9 +8,15 @@ public class UIManager : MonoBehaviour
     public Text timerText;
     public int timer;
 
+    public Text playerSkillText;
+    public Text difficultyText;
+
     void Update()
     {
         timer = (int)GetComponent<GameManager>().timer;
         timerText.text = $"Countdown:\n{timer.ToString()} seconds";
+
+        playerSkillText.text = $"Player Skill:\n{GetComponent<GameManager>().playerSkill.ToString()}";
+        difficultyText.text = $"Hack Level:\n{GetComponent<GameManager>().difficultylvl.ToString()}";
     }
 }
