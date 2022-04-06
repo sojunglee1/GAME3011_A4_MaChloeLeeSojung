3cd5adb025f0340a47e26d6ff250d5fae02de524
Sojung Lee <sojung.lee2@georgebrown.ca>
2022-04-06 11:24:08 -0400

    Added Difficulty Level & Buttons Change Color

---
+++ GAME3011_A4/Assets/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum DifficultyLevel
+{
+    Easy,
+    Medium,
+    Hard
+}
+
+public class GameManager : MonoBehaviour
+{
+    public DifficultyLevel difficultylvl;
+    public int blockedTile;
+    public List<Tile> buttons;
+
+    private void Awake()
+    {
+        Random.seed = Random.Range(0, 100);
+    }
+    public void Start()
+    {
+        foreach(Tile button in buttons)
+        {
+            button.ResetTile();
+        }
+        SetDifficultyLevel();
+        ResetBlockedTiles();
+    }
+
+    void ResetBlockedTiles()
+    {
+        int index = Random.Range(0, buttons.Count - 1);
+        print(index);
+        for (int i = 0; i < blockedTile; i++)
+        {
+            if (!buttons[index].blocked)
+            {
+                buttons[index].blocked = true;
+            }
+            else
+            {
+                if (index < buttons.Count) buttons[index++].blocked = true;
+                else buttons[index--].blocked = true;
+            }
+        }
+    }
+
+    void SetDifficultyLevel()
+    {
+        switch (difficultylvl)
+        {
+            case DifficultyLevel.Easy:
+                blockedTile = 1;
+                break;
+
+            case DifficultyLevel.Medium:
+                blockedTile = 2;
+                break;
+
+            case DifficultyLevel.Hard:
+                blockedTile = 3;
+                break;
+
+            default:
+                blockedTile = 0;
+                break;
+        }
+    }
+}
---
+++ GAME3011_A4/Assets/Tile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Tile : MonoBehaviour
+{
+    Button button;
+    public bool selected = false;
+    public bool blocked = false;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+
+    }
+
+    private void Update()
+    {
+        if (blocked)
+        {
+            GetComponent<Image>().color = Color.red;
+        }
+    }
+
+    public void Toggle()
+    {
+        selected = !selected;
+
+        if (selected)
+        {
+            GetComponent<Image>().color = Color.green;
+        }
+    }
+
+    public void ResetTile()
+    {
+        blocked = false;
+        selected = false;
+
+        GetComponent<Image>().color = Color.cyan;
+    }
+}
